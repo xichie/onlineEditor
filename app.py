920e06f4895fdd6da63943803c2d3d45cfd596e7
warm <warm@LAPTOP-UHF31VRT.localdomain>
2020-08-14 09:06:05 +0800

    端口设置

--- app.py
+++ app.py
@@ -258,7 +258,7 @@ def main():
     if args.version:
         print(__version__)
         exit(0)
-    print("serving on http://127.0.0.1:" + args.port)
+    print("serving on http://127.0.0.1:" , args.port)
     app.config["cmd"] = [args.command] + shlex.split(args.cmd_args)
     socketio.run(app, host='0.0.0.0', debug=args.debug, port=int(args.port))
     
